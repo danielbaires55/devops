65e30e0b1b754e7393457b65e72fb44346cb07d9
root <root@DESKTOP-KQRALU3>
2024-12-10 17:49:50 +0100

    feat: exec sum

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-import { formatLower } from './lib/formatters.js';
-const testString = "   Hello World!   ";
-const formattedString = formatLower(testString);
+// import { formatLower } from './lib/formatters.js';
+// const testString = "   Hello World!   ";
+// const formattedString = formatLower(testString);
 
-console.log(`Original: "${testString}"`);
-console.log(`Formatted: "${formattedString}"`);
+// console.log(`Original: "${testString}"`);
+// console.log(`Formatted: "${formattedString}"`);
 
 // Output atteso:
 // Original: "   Hello World!   "
 // Formatted: "hello world!"
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(6, 19));
