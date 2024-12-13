aa7859102d764de6c7e7cc7fc8a4b88396901849
root <root@DESKTOP-KQRALU3>
2024-12-13 13:48:41 +0100

    feat: aggiunta documentazione gamble game

--- games.js
+++ games.js
@@ -1,4 +1,14 @@
 // games.js
+/**
+ * La funzione gamble genera un numero random tra 1 e 10
+ * e valuta quanto il numero fornito in input è vicino al
+ * numero generato. Restituisce:
+ * - "Jackpot" se i numeri sono identici.
+ * - "Quasi" se la differenza è entro 3.
+ * - "Mannaggia" altrimenti.
+ * @param {number} number - Numero compreso tra 1 e 10 fornito in input
+ * @returns {string} - Risultato della valutazione
+ */
 function gamble(number) {
     const random = Math.floor(Math.random() * 10) + 1;
 
@@ -10,4 +20,3 @@ function gamble(number) {
         return "Mannaggia";
     }
 }
-
