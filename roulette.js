9855c5a2058680410d92c2a7bfe8b6a9a10ff1fc
root <root@DESKTOP-KQRALU3>
2024-12-13 14:24:48 +0100

    feat: logica vincita

--- roulette.js
+++ roulette.js
@@ -1,4 +1,11 @@
 function spin(number) {
-    const random = Math.floor(Math.random() * 37); // Genera un numero da 0 a 36
-    return random;
+    const random = Math.floor(Math.random() * 37);
+
+    if (number === random) {
+        return "Hai vinto al 100%!";
+    } else if (Math.floor(number / 10) === Math.floor(random / 10)) {
+        return "Vicino, ma non abbastanza.";
+    } else {
+        return "Riprova la fortuna!";
+    }
 }
