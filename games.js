f2055841b118f21208224febd0d5c2ae91720f03
root <root@DESKTOP-KQRALU3>
2024-12-13 13:48:23 +0100

    feat: logica gioco

--- games.js
+++ games.js
@@ -1,5 +1,13 @@
 // games.js
 function gamble(number) {
     const random = Math.floor(Math.random() * 10) + 1;
-    return random;
+
+    if (number === random) {
+        return "Jackpot";
+    } else if (Math.abs(number - random) <= 3) {
+        return "Quasi";
+    } else {
+        return "Mannaggia";
+    }
 }
+
