305e09dd6a892309b1cd776c49a304ded7140338
root <root@DESKTOP-KQRALU3>
2024-12-13 14:25:23 +0100

    feat: documentazione funzione spin

--- roulette.js
+++ roulette.js
@@ -1,4 +1,14 @@
-function spin(number) {
+/**
+ * La funzione spin accetta un numero intero tra 0 e 36 come input e lo confronta con un numero casuale
+ * generato nello stesso intervallo. Restituisce:
+ * - "Hai vinto al 100%!" se il numero generato corrisponde all'input.
+ * - "Vicino, ma non abbastanza." se i numeri appartengono alla stessa decina.
+ * - "Riprova la fortuna!" in tutti gli altri casi.
+ * 
+ * @param {number} number - Numero compreso tra 0 e 36 fornito in input
+ * @returns {string} - Risultato del confronto
+ */
+export function spin(number) {
     const random = Math.floor(Math.random() * 37);
 
     if (number === random) {
